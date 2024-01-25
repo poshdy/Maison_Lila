b09f0aa634dbf8c1d34ebe37b771bc8086f0de0e
Roshdy <126667610+poshdy@users.noreply.github.com>
2024-01-25 07:29:45 -0800

    "add cors"

--- server/src/server.ts
+++ server/src/server.ts
@@ -33,7 +33,7 @@ const app = express();
 app.use(helmet());
 app.use(
   cors({
-    origin: ["http://maisonlila.shop", "http://api.maisonlila.shop"],
+    origin: ["http://maisonlila.shop", "http://admin.maisonlila.shop","www.maisonlila.shop"],
     methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
     credentials: true,
     optionsSuccessStatus: 204,
